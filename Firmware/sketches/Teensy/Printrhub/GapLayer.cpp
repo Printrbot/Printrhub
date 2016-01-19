f9d7406feafe9d834826544b54266426a54fe170
Phillip Schuster <pschuster@me.com>
2016-01-20 00:25:43 +0100

    Removed line stroke from Gap Layer (very useful to debug display segmentation)

--- Firmware/sketches/Teensy/Printrhub/GapLayer.cpp
+++ Firmware/sketches/Teensy/Printrhub/GapLayer.cpp
@@ -11,7 +11,7 @@ GapLayer::GapLayer(Rect frame): RectangleLayer(frame)
 {
     this->setBackgroundColor(ILI9341_WHITE);
     this->setStrokeColor(ILI9341_BLACK);
-    this->setStrokeWidth(1);
+    this->setStrokeWidth(0);
     globR += 20;
 }
 
