afd9ae46cbfb64bebf08b505b017a0a6fec2110a
mick <mick@newmediafront.com>
2016-09-02 13:32:36 -0700

    added G2 reset and erase and cts/rts pins

--- mk20/src/framework/core/HAL.h
+++ mk20/src/framework/core/HAL.h
@@ -10,6 +10,9 @@
 
 #ifdef PRINTRHUB_REV_01
 
+#define CTS_PIN 20
+#define RTS_PIN 6
+
 //General setup
 #define LED_PIN 21
 
@@ -28,6 +31,9 @@
 #define COMMSTACK_DATALOSS_MARKER_PIN 3
 #define COMMSTACK_BAUDRATE 1728000
 
+#define G2_RESET_PIN 27
+#define G2_ERASE_PIN 28
+
 //Debug
 #define DebugSerial Serial
 
@@ -38,6 +44,10 @@
 //General setup
 #define LED_PIN 21
 
+#define CTS_PIN 20
+#define RTS_PIN 6
+
+
 //The display also uses hardware SPI, plus #9 & #10
 #define TFT_CS 10
 #define TFT_DC 9
@@ -47,6 +57,9 @@
 #define TFT_SCLK 13
 #define TFT_TOUCH_SENSE_PIN 17
 #define TFT_BACKLIGHT_PWM 22
+
+#define G2_RESET_PIN 27
+#define G2_ERASE_PIN 28
 
 //Commstack
 #define COMMSTACK_DATAFLOW_PIN 31
