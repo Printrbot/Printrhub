ff22a989335f249fc654c7e35cc207907a5bb658
mick <mick@newmediafront.com>
2016-09-12 20:17:04 -0700

    updated layout

--- mk20/src/scenes/projects/ConfirmDeleteProject.cpp
+++ mk20/src/scenes/projects/ConfirmDeleteProject.cpp
@@ -37,25 +37,26 @@ uint16_t ConfirmDeleteProject::getBackgroundColor()
 
 void ConfirmDeleteProject::onWillAppear() {
 
-  BitmapView* icon = new BitmapView(Rect(100,24,uiBitmaps.icon_alert.width, uiBitmaps.icon_alert.height));
+  BitmapView* icon = new BitmapView(Rect(90,20,uiBitmaps.icon_alert.width, uiBitmaps.icon_alert.height));
   icon->setBitmap(&uiBitmaps.icon_alert);
   addView(icon);
 
+/*
   TextLayer* textLayer = new TextLayer(Rect(34,118, 197, 20));
   textLayer->setFont(&LiberationSans_14);
   textLayer->setTextAlign(TEXTALIGN_CENTERED);
   textLayer->setForegroundColor(ILI9341_WHITE);
   textLayer->setText("Are you sure");
   Display.addLayer(textLayer);
-
-  _yesBtn = new BitmapButton(Rect(50,178, uiBitmaps.btn_yes.width, uiBitmaps.btn_yes.height));
-  _yesBtn->setBitmap(&uiBitmaps.btn_yes);
+*/
+  _yesBtn = new BitmapButton(Rect(17,112, uiBitmaps.btn_delete_project.width, uiBitmaps.btn_delete_project.height));
+  _yesBtn->setBitmap(&uiBitmaps.btn_delete_project);
   _yesBtn->setDelegate(this);
   addView(_yesBtn);
 
-  _noBtn = new BitmapButton(Rect(137,178, uiBitmaps.btn_no.width, uiBitmaps.btn_no.height));
+  _noBtn = new BitmapButton(Rect(17,172, uiBitmaps.btn_cancel.width, uiBitmaps.btn_cancel.height));
   _noBtn->setDelegate(this);
-  _noBtn->setBitmap(&uiBitmaps.btn_no);
+  _noBtn->setBitmap(&uiBitmaps.btn_cancel);
   addView(_noBtn);
 
   SidebarSceneController::onWillAppear();
