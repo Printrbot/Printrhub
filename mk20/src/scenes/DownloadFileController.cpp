18eccbe3b92ddd6be7799a2973463d09c657785f
mick <mick@newmediafront.com>
2016-09-28 09:27:17 -0700

    changed progress bar color (not working) and updated white space formatting (spaces vs tabs)

--- mk20/src/scenes/DownloadFileController.cpp
+++ mk20/src/scenes/DownloadFileController.cpp
@@ -16,21 +16,21 @@
 extern UIBitmaps uiBitmaps;
 
 DownloadFileController::DownloadFileController(String url, String localFilePath) :
-	SidebarSceneController::SidebarSceneController(),
-	_fileSize(0),
-	_bytesRead(0),
-	_previousPercent(0),
-	_localFilePath(localFilePath),
-	_url(url)
+  SidebarSceneController::SidebarSceneController(),
+  _fileSize(0),
+  _bytesRead(0),
+  _previousPercent(0),
+  _localFilePath(localFilePath),
+  _url(url)
 {
 
 }
 
 DownloadFileController::DownloadFileController() :
-	SidebarSceneController::SidebarSceneController(),
-	_fileSize(0),
-	_bytesRead(0),
-	_previousPercent(0)
+  SidebarSceneController::SidebarSceneController(),
+  _fileSize(0),
+  _bytesRead(0),
+  _previousPercent(0)
 {
 
 }
@@ -42,12 +42,12 @@ DownloadFileController::~DownloadFileController()
 
 uint16_t DownloadFileController::getBackgroundColor()
 {
-	return Application.getTheme()->getColor(BackgroundColor);
+  return Application.getTheme()->getColor(BackgroundColor);
 }
 
 String DownloadFileController::getName()
 {
-	return "DownloadFileController";
+  return "DownloadFileController";
 }
 
 
@@ -63,172 +63,166 @@ UIBitmap * DownloadFileController::getSidebarBitmap() {
 
 void DownloadFileController::onWillAppear()
 {
-	BitmapView* icon = new BitmapView(Rect(0,0,uiBitmaps.downloading_scene.width, uiBitmaps.downloading_scene.height));
+  BitmapView* icon = new BitmapView(Rect(0,0,uiBitmaps.downloading_scene.width, uiBitmaps.downloading_scene.height));
   icon->setBitmap(&uiBitmaps.downloading_scene);
   addView(icon);
-/*
-	TextLayer* textLayer = new TextLayer(Rect(5, 200, 260, 20));
-  textLayer->setFont(&LiberationSans_12);
-  textLayer->setTextAlign(TEXTALIGN_CENTERED);
-  textLayer->setForegroundColor(ILI9341_WHITE);
-  textLayer->setText("Downloading, please wait...");
-  Display.addLayer(textLayer);
-*/
-	_progressBar = new ProgressBar(Rect(0,235,270,5));
-	_progressBar->setTrackColor(ILI9341_WHITE);
-	_progressBar->setValue(0.0f);
-	addView(_progressBar);
 
-	//Trigger file download
-	const char* url = _url.c_str();
-	Application.getESPStack()->requestTask(TaskID::DownloadFile,strlen(url),(uint8_t*)url);
+  _progressBar = new ProgressBar(Rect(0,228,270,12));
+  //_progressBar->setTrackColor(ILI9341_WHITE);
+	_progressBar->setTrackColor(Application.getTheme()->getColor(HighlightBackgroundColor));
+  _progressBar->setValue(0.0f);
+  addView(_progressBar);
 
-	SidebarSceneController::onWillAppear();
+  //Trigger file download
+  const char* url = _url.c_str();
+  Application.getESPStack()->requestTask(TaskID::DownloadFile,strlen(url),(uint8_t*)url);
+
+  SidebarSceneController::onWillAppear();
 }
 
 void DownloadFileController::onSidebarButtonTouchUp()
 {
-	ProjectsScene* scene = new ProjectsScene();
-	Application.pushScene(scene);
+  ProjectsScene* scene = new ProjectsScene();
+  Application.pushScene(scene);
 }
 
 bool DownloadFileController::handlesTask(TaskID taskID)
 {
-	switch(taskID) {
-		case TaskID::GetJobWithID:
-		case TaskID::FileSaveData:
-		case TaskID::FileClose:
-		case TaskID::SaveProjectWithID:
-		case TaskID::FileSetSize:
-		case TaskID::Error:
-		case TaskID::DownloadFile:
-			return true;
-			break;
-		default:
-			return false;
-	}
+  switch(taskID) {
+    case TaskID::GetJobWithID:
+    case TaskID::FileSaveData:
+    case TaskID::FileClose:
+    case TaskID::SaveProjectWithID:
+    case TaskID::FileSetSize:
+    case TaskID::Error:
+    case TaskID::DownloadFile:
+      return true;
+      break;
+    default:
+      return false;
+  }
 }
 
 bool DownloadFileController::runTask(CommHeader &header, const uint8_t *data, size_t dataSize, uint8_t *responseData, uint16_t *responseDataSize, bool* sendResponse, bool* success)
 {
-	LOG_VALUE("DownloadFileController handling task",header.getCurrentTask());
+  LOG_VALUE("DownloadFileController handling task",header.getCurrentTask());
 
-	if (header.getCurrentTask() == TaskID::FileSetSize) {
-		uint32_t contentLength;
-		memcpy(&contentLength,data,sizeof(uint32_t));
-		_fileSize = contentLength;
-		_bytesRead = 0;
-		*sendResponse = true;
-		*responseDataSize = 0;
+  if (header.getCurrentTask() == TaskID::FileSetSize) {
+    uint32_t contentLength;
+    memcpy(&contentLength,data,sizeof(uint32_t));
+    _fileSize = contentLength;
+    _bytesRead = 0;
+    *sendResponse = true;
+    *responseDataSize = 0;
 
-	}
-	else if (header.getCurrentTask() == TaskID::DownloadFile)
-	{
-		LOG("Handling GetJobWithID Task");
+  }
+  else if (header.getCurrentTask() == TaskID::DownloadFile)
+  {
+    LOG("Handling GetJobWithID Task");
 
-		if (header.commType == ResponseSuccess)
-		{
-			if (dataSize != sizeof(uint32_t))
-			{
-				LOG_VALUE("Expected content of GetJobWithID to be uint32_t with the number of bytes to receive, but received a content length of",*responseDataSize);
-			}
-			else
-			{
-				uint32_t contentLength;
-				memcpy(&contentLength,data,sizeof(uint32_t));
-				_fileSize = contentLength;
-				_bytesRead = 0;
+    if (header.commType == ResponseSuccess)
+    {
+      if (dataSize != sizeof(uint32_t))
+      {
+        LOG_VALUE("Expected content of GetJobWithID to be uint32_t with the number of bytes to receive, but received a content length of",*responseDataSize);
+      }
+      else
+      {
+        uint32_t contentLength;
+        memcpy(&contentLength,data,sizeof(uint32_t));
+        _fileSize = contentLength;
+        _bytesRead = 0;
 
-				LOG_VALUE("Expected file size of",_fileSize);
+        LOG_VALUE("Expected file size of",_fileSize);
 
-				//Open a file on SD card
-				//char * fp[_localFilePath.length() + 1];
-				//_localFilePath.toCharArray(fp, _localFilePath.length());
-				//SD.remove(fp);
-				_file = SD.open(_localFilePath.c_str(),FILE_WRITE);
-				if (!_file.available())
-				{
-					//TODO: We should handle that. For now we will have to read data from ESP to clean the pipe but there should be better ways to handle errors
-					//Application.getESPStack()->requestTask(Error);
-					//return false;
-				}
+        //Open a file on SD card
+        //char * fp[_localFilePath.length() + 1];
+        //_localFilePath.toCharArray(fp, _localFilePath.length());
+        //SD.remove(fp);
+        _file = SD.open(_localFilePath.c_str(),FILE_WRITE);
+        if (!_file.available())
+        {
+          //TODO: We should handle that. For now we will have to read data from ESP to clean the pipe but there should be better ways to handle errors
+          //Application.getESPStack()->requestTask(Error);
+          //return false;
+        }
 
-				LOG("File opened for writing. Now waiting for number of bytes to read");
-				LOG("Now Expecting file chunks with FileSaveData tasks");
-			}
-		}
-		else
-		{
-			//TODO: Show an error message
-			ProjectsScene* scene = new ProjectsScene();
-			Application.pushScene(scene);
-		}
-	}
-	else if (header.getCurrentTask() == TaskID::FileSaveData)
-	{
-		LOG("Handling FileSaveData Task");
-		if (header.commType == Request)
-		{
-			LOG_VALUE("Received Chunk of Data with Size", dataSize);
-			int numBytesWritten = _file.write(data, dataSize);
-			LOG_VALUE("Written number of bytes to file",numBytesWritten);
+        LOG("File opened for writing. Now waiting for number of bytes to read");
+        LOG("Now Expecting file chunks with FileSaveData tasks");
+      }
+    }
+    else
+    {
+      //TODO: Show an error message
+      ProjectsScene* scene = new ProjectsScene();
+      Application.pushScene(scene);
+    }
+  }
+  else if (header.getCurrentTask() == TaskID::FileSaveData)
+  {
+    LOG("Handling FileSaveData Task");
+    if (header.commType == Request)
+    {
+      LOG_VALUE("Received Chunk of Data with Size", dataSize);
+      int numBytesWritten = _file.write(data, dataSize);
+      LOG_VALUE("Written number of bytes to file",numBytesWritten);
 
-			*sendResponse = true;
-			*responseDataSize = 0;
+      *sendResponse = true;
+      *responseDataSize = 0;
 
-			//Add number of bytes received to total bytes read
-			_bytesRead += dataSize;
+      //Add number of bytes received to total bytes read
+      _bytesRead += dataSize;
 
-			float fraction = (float)_bytesRead / (float)_fileSize;
-			int percent = (int)(fraction*100.0f);
+      float fraction = (float)_bytesRead / (float)_fileSize;
+      int percent = (int)(fraction*100.0f);
 
-			if (percent != _previousPercent)
-			{
-				if (percent % 5 == 0)
-				{
-					//Only update the progress bar once in a while as the communication is blocked while doing so and we don't want to loose too much speed
+      if (percent != _previousPercent)
+      {
+        if (percent % 5 == 0)
+        {
+          //Only update the progress bar once in a while as the communication is blocked while doing so and we don't want to loose too much speed
 
-					// TODO: Currently locks up the communication, disabled until fixed
-					_progressBar->setValue(fraction);
-				}
-			}
+          // TODO: Currently locks up the communication, disabled until fixed
+          _progressBar->setValue(fraction);
+        }
+      }
 
-			_previousPercent = percent;
+      _previousPercent = percent;
 
-		}
-	}
-	else if (header.getCurrentTask() == TaskID::FileClose)
-	{
-		LOG("Handling FileClose Task");
-		LOG_VALUE("Bytes read",_bytesRead);
-		_file.close();
+    }
+  }
+  else if (header.getCurrentTask() == TaskID::FileClose)
+  {
+    LOG("Handling FileClose Task");
+    LOG_VALUE("Bytes read",_bytesRead);
+    _file.close();
 
-		//PrintStatusSceneController* scene = new PrintStatusSceneController();
-		ProjectsScene* scene = new ProjectsScene();
-		Application.pushScene(scene);
-	}
-	else if (header.getCurrentTask() == TaskID::SaveProjectWithID)
-	{
-		char _fp[header.contentLength+1];
-		memset(_fp, 0, header.contentLength+1);
-		memcpy(_fp, data, header.contentLength);
-		_localFilePath = String(_fp);
-		_localFilePath = "/projects/" + _localFilePath;
-		//Open a file on SD card
-		SD.remove(_fp);
-		//SD.remove(_localFilePath.c_str());
-		_file = SD.open(_localFilePath.c_str(), FILE_WRITE);
+    //PrintStatusSceneController* scene = new PrintStatusSceneController();
+    ProjectsScene* scene = new ProjectsScene();
+    Application.pushScene(scene);
+  }
+  else if (header.getCurrentTask() == TaskID::SaveProjectWithID)
+  {
+    char _fp[header.contentLength+1];
+    memset(_fp, 0, header.contentLength+1);
+    memcpy(_fp, data, header.contentLength);
+    _localFilePath = String(_fp);
+    _localFilePath = "/projects/" + _localFilePath;
+    //Open a file on SD card
+    SD.remove(_fp);
+    //SD.remove(_localFilePath.c_str());
+    _file = SD.open(_localFilePath.c_str(), FILE_WRITE);
 
-		*sendResponse = true;
-		*responseDataSize = 0;
-	}
+    *sendResponse = true;
+    *responseDataSize = 0;
+  }
 
-	return true;
+  return true;
 }
 
 #pragma mark ButtonDelegate Implementation
 
 void DownloadFileController::buttonPressed(void *button)
 {
-	SidebarSceneController::buttonPressed(button);
+  SidebarSceneController::buttonPressed(button);
 }
