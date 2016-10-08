4e6ec80600ce12d3f87fc6b5493a910f120a038c
Phillip Schuster <pschuster@me.com>
2016-10-08 18:07:11 +0200

    Added Rev 0.4 settings and activated Rev 0.4

--- esp/src/hal.h
+++ esp/src/hal.h
@@ -6,7 +6,8 @@
 #define ESP8266_ARM_SWD_HAL_H
 
 //#define PRINTRHUB_REV_01    //Uncomment if building the code for Printrhub Rev 0.1
-#define PRINTRHUB_REV_02  //Uncomment if building the code for Printrhub Rev 0.2
+//#define PRINTRHUB_REV_02  //Uncomment if building the code for Printrhub Rev 0.2
+#define PRINTRHUB_REV_04  //Uncomment if building the code for Printrhub Rev 0.4
 
 #ifdef PRINTRHUB_REV_01
 
@@ -24,6 +25,21 @@
 #endif
 
 #ifdef PRINTRHUB_REV_02
+
+//Firmware updates for MK20
+#define MK20_RESET_PIN 13
+#define MK20_SWD_CLK 5
+#define MK20_SWD_IO 14
+
+//Commstack
+#define COMMSTACK_DATAFLOW_PIN 4
+#define COMMSTACK_BAUDRATE 115200
+#define COMMSTACK_WORKING_MARKER_PIN 16
+#define COMMSTACK_INFO_MARKER_PIN 12
+
+#endif
+
+#ifdef PRINTRHUB_REV_04
 
 //Firmware updates for MK20
 #define MK20_RESET_PIN 13
--- mk20/src/framework/core/HAL.h
+++ mk20/src/framework/core/HAL.h
@@ -6,8 +6,9 @@
 #define TEENSY_HAL_H
 
 //#define PRINTRHUB_REV_01    //Uncomment if building the code for Printrhub Rev 0.1
-#define PRINTRHUB_REV_02  //Uncomment if building the code for Printrhub Rev 0.2
+//#define PRINTRHUB_REV_02  //Uncomment if building the code for Printrhub Rev 0.2
 //#define PRINTRHUB_REV_03    //Uncomment if building the code for Printrhub Rev 0.3
+#define PRINTRHUB_REV_04    //Uncomment if building the code for Printrhub Rev 0.3
 
 #ifdef PRINTRHUB_REV_01
 
@@ -16,6 +17,9 @@
 
 //General setup
 #define LED_PIN 21
+
+//Connection to ESP
+#define ESP_RESET 5
 
 //The display also uses hardware SPI, plus #9 & #10
 #define TFT_CS 10
@@ -45,6 +49,9 @@
 //General setup
 #define LED_PIN 21
 
+//Connection to ESP
+#define ESP_RESET 5
+
 #define CTS_PIN 20
 #define RTS_PIN 6
 
@@ -65,7 +72,7 @@
 //Commstack
 #define COMMSTACK_DATAFLOW_PIN 31
 #define COMMSTACK_DATALOSS_MARKER_PIN 3
-#define COMMSTACK_BAUDRATE 1728000
+#define COMMSTACK_BAUDRATE 115200
 
 //Debug
 #define DEBUG_USE_SOFTWARE_SERIAL
@@ -79,6 +86,9 @@
 
 //General setup
 #define LED_PIN 21
+
+//Connection to ESP
+#define ESP_RESET 5
 
 //The display also uses hardware SPI, plus #9 & #10
 #define TFT_CS 10
@@ -108,6 +118,41 @@
 #define DEBUG_SOFTWARE_SERIAL_RX_PIN 14
 #define DEBUG_SOFTWARE_SERIAL_TX_PIN 2
 #define DebugSerial DebugSerial
+
+#endif
+
+#ifdef PRINTRHUB_REV_04
+
+#define CTS_PIN 20
+#define RTS_PIN 6
+
+//General setup
+#define LED_PIN 21
+
+//Connection to ESP
+#define ESP_RESET 5
+
+//The display also uses hardware SPI, plus #9 & #10
+#define TFT_CS 10
+#define TFT_DC 9
+#define TFT_RST 0
+#define TFT_MOSI 11
+#define TFT_MISO 12
+#define TFT_SCLK 13
+#define TFT_TOUCH_SENSE_PIN 17
+#define TFT_BACKLIGHT_PWM 22
+
+//Commstack
+#define COMMSTACK_DATAFLOW_PIN 24
+#define COMMSTACK_DATALOSS_MARKER_PIN 3
+#define COMMSTACK_BAUDRATE 1728000
+
+#define G2_RESET_PIN 27
+#define G2_ERASE_PIN 28
+
+//Debug
+#define DebugSerial Serial2
+#define DEBUG_SETUP CORE_PIN9_CONFIG = 0; CORE_PIN10_CONFIG = 0; CORE_PIN26_CONFIG = PORT_PCR_PE | PORT_PCR_PS | PORT_PCR_PFE | PORT_PCR_MUX(3); CORE_PIN31_CONFIG = PORT_PCR_DSE | PORT_PCR_SRE | PORT_PCR_MUX(3);
 
 #endif
 
