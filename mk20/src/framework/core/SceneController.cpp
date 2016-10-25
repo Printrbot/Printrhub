99305da9e385121677f360d91f89dce5565afdf2
Phillip Schuster <pschuster@me.com>
2016-10-25 23:13:04 +0200

    Removed old printrCallback which is now replaced with a more formal class interface.

--- mk20/src/framework/core/SceneController.cpp
+++ mk20/src/framework/core/SceneController.cpp
@@ -343,10 +343,6 @@ void SceneController::animationUpdated(Animation *animation, float currentValue,
 	}
 }
 
-void SceneController::printrCallback(const char ctype[], float * fdata, int * idata) {
-}
-
-
 void SceneController::animationFinished(Animation *animation)
 {
 	if (animation == _scrollAnimation)
