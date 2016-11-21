a0ffd9a1f472c0bb5a6725630a8a203efdbc67ca
Phillip Schuster <pschuster@me.com>
2016-11-21 12:48:00 +0100

    Removed older code from main.cpp

--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -25,22 +25,12 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#include <EEPROM.h>
-#include <ESP8266WiFi.h>
-#include <Wire.h>
 #include <SPI.h>
 #include "application.h"
-#include "core/CommStack.h"
-#include "core/Mode.h"
-
 
 extern "C" {
   #include "user_interface.h"
 }
-
-//Net net;
-//WebServer web;
-
 
 void setup() {
   system_update_cpu_freq(160);
