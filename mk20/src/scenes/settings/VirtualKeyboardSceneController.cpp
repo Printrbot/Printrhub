bbae22848d2a69fe118065b4c37f7064f3ee6d8f
Phillip Schuster <pschuster@me.com>
2018-02-09 09:54:07 +0100

    Fixed a compilation bug with characters in keyboard that don’t fit in a char.

--- mk20/src/scenes/settings/VirtualKeyboardSceneController.cpp
+++ mk20/src/scenes/settings/VirtualKeyboardSceneController.cpp
@@ -36,9 +36,9 @@
 #include "../../../lib/fonts/font_AwesomeF000.h"
 #include "framework/views/Button.h"
 
-const char Keyboards[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','.',',',';','-','_','@',
+const unsigned char Keyboards[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','.',',',';','-','_','@',
                           'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','.',',',';','-','_','@',
-                          '0','1','2','3','4','5','6','7','8','9','^','°','!','"','§','$','%','&','/','(',')','=','?','´','`','+','-','#','\'',':','<','>'};
+                          '0','1','2','3','4','5','6','7','8','9','!','"','$','%','&','/','(',')','=','?','+','-','#','\'',':','<','>'};
 
 VirtualKeyboardSceneController::VirtualKeyboardSceneController(VirtualKeyboardHandler *handler, const char *name) :
 	_handler(handler),
